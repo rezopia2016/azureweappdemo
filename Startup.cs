563122050e76d92ee10e1b965365e3ef79c4609c
Santhosh Kumar S <santhosh.kumar.s@accenture.com>
2021-01-08 17:50:23 +0530

    push to remote

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace AzureWebapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World Redeploy!");
                 });
             });
         }
